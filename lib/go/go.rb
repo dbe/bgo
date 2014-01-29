eeb14ed3e10978dc57a43b87406b7a791cb57d4c
Brian Ethier <davidbrianethier@gmail.com>
2014-01-29 23:11:31 +0700

    Adding more methods, adding more tests

--- lib/go/go.rb
+++ lib/go/go.rb
@@ -13,31 +13,31 @@ class GnuGo
   #TODO: make private
   def send_message(message)
     @io.puts(message)
-    ret = @io.gets()
 
-    if ret == "? unknown command\n"
-      raise UnknownCommandError
+    resp = @io.gets()
+    @io.gets() #Throw away second newline
+
+    if resp[0] == '?'
+      if resp == "? unknown command\n"
+        raise UnknownCommandError
+      end
+    else
+      resp = parse_sucessful_response(resp)
     end
 
-    return ret
+    return resp
+  end
+
+  def parse_sucessful_response(response)
+    return response[1..-1].strip()
   end
 
   def get_komi()
     return send_message('get_komi')
   end
+
+  def set_komi(komi)
+    send_message("komi #{komi}")
+  end
 end
-
-
-#puts "First"
-#gg = GnuGo.new()
-
-#puts "Second"
-#puts gg.get_komi()
-
-#puts "Third"
-#puts gg.fuck().inspect
-
-#puts "Exit"
-
-
 
--- test/unit/go/go_test.rb
+++ test/unit/go/go_test.rb
@@ -9,4 +9,11 @@ class GoTest < ActionController::TestCase
       gg.send_message("I'm not a real command")
     end
   end
+
+  test "set_komi works" do
+    gg = GnuGo.new()
+    assert_equal("0.0", gg.get_komi())
+    gg.set_komi("5.0")
+    assert_equal("5.0", gg.get_komi())
+  end
 end
